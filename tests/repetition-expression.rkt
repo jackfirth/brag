2fe5fb82386c781b68cdd78327a6a8669b9a8c51
Jack Firth <jackhfirth@gmail.com>
2023-06-13 20:10:28 -0700

    Add more failing tests for repetition

--- tests/repetition-expression.rkt
+++ tests/repetition-expression.rkt
@@ -15,23 +15,70 @@
 
 
 (module+ test
-  (test-case "repetition-expression")
   (define start (nonterminal-symbol 'start))
+  (define s1 (nonterminal-symbol 's1))
   (define number (atom-symbol 'number))
-  (define test-grammar
-    (grammar
-     #:start-symbol start
-     #:rules
-     (list
-      (production-rule
-       #:nonterminal start
-       #:action (build-list-action)
-       #:substitution (repetition-expression number)))))
-  (define parser (earley-parser test-grammar))
-  (define input
-    (list (atom 'number 1)
-          (atom 'number 2)
-          (atom 'number 3)
-          (atom 'number 4)
-          (atom 'number 5)))
-  (check-equal? (parse-datum parser input) '(1 2 3 4 5)))
+
+  (test-case "manual repetition with right recursion"
+    (define g
+      (grammar
+       #:start-symbol start
+       #:rules
+       (list
+        (production-rule #:nonterminal start #:action (build-list-action) #:substitution s1)
+        (production-rule
+         #:nonterminal s1 #:action splice-action #:substitution (group-expression '()))
+        (production-rule
+         #:nonterminal s1
+         #:action splice-action
+         #:substitution (group-expression (list number s1))))))
+    (define parser (earley-parser g))
+    (define input
+      (list (atom 'number 1)
+            (atom 'number 2)
+            (atom 'number 3)
+            (atom 'number 4)
+            (atom 'number 5)))
+    (check-equal? (parse-datum parser input) '(1 2 3 4 5)))
+
+  (test-case "manual repetition with left recursion"
+    (define g
+      (grammar
+       #:start-symbol start
+       #:rules
+       (list
+        (production-rule #:nonterminal start #:action (build-list-action) #:substitution s1)
+        (production-rule
+         #:nonterminal s1 #:action splice-action #:substitution (group-expression '()))
+        (production-rule
+         #:nonterminal s1
+         #:action splice-action
+         #:substitution (group-expression (list s1 number))))))
+    (define parser (earley-parser g))
+    (define input
+      (list (atom 'number 1)
+            (atom 'number 2)
+            (atom 'number 3)
+            (atom 'number 4)
+            (atom 'number 5)))
+    (check-equal? (parse-datum parser input) '(1 2 3 4 5)))
+
+  (test-case "repetition-expression"
+    (define g
+      (grammar
+       #:start-symbol start
+       #:rules
+       (list
+        (production-rule
+         #:nonterminal start
+         #:action (build-list-action)
+         #:substitution (repetition-expression number)))))
+    (define parser (earley-parser g))
+    (define input
+      (list (atom 'number 1)
+            (atom 'number 2)
+            (atom 'number 3)
+            (atom 'number 4)
+            (atom 'number 5)))
+    (check-equal? (parse-datum parser input) '(1 2 3 4 5))))
+  
