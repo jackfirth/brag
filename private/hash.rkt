6d912b113ad08a7540e3b4e922bbd6f69de6563b
Jack Firth <jackhfirth@gmail.com>
2023-08-31 22:42:27 -0700

    Update hash.rkt

--- private/hash.rkt
+++ private/hash.rkt
@@ -12,8 +12,9 @@
 ;@----------------------------------------------------------------------------------------------------
 
 
-(define (hash->immutable-hash h)
-  (if (and (hash? h) (immutable? h))
-      h
-      (for/hash ([(k v) (in-hash h)])
-        (values k v))))
+(define hash->immutable-hash
+  (lambda (h)
+    (if (and (hash? h) (immutable? h))
+        h
+        (for/hash ([(k v) (in-hash h)])
+          (values k v)))))
