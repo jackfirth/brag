67f0ff8d6ac51b15ecb29ae873687d1ec17c56c3
Jack Firth <jackhfirth@gmail.com>
2023-09-01 18:06:10 -0700

    Update resyntax-test-fodder.rkt

--- private/resyntax-test-fodder.rkt
+++ private/resyntax-test-fodder.rkt
@@ -5,6 +5,11 @@
   (lambda (x)
     (println "before bar definition")
     (define (bar)
-      (+ 1 2 3))
+      (+ 1 2 3 4 5 6))
     (println "after bar definition")
     (bar)))
+
+
+(define baz
+  (lambda (x y z)
+    42))
